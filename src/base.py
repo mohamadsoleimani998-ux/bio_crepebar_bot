535a8d814df17aec8af0221e81b471135b41162e
mohamadsoleimani998-ux <mohamadsoleimani998@gmail.com>
2025-08-10 00:16:51 +0330

    Update base.py

--- src/base.py
+++ src/base.py
@@ -2,11 +2,12 @@ import os
 import httpx
 
 TOKEN = os.getenv("BOT_TOKEN")
-BASE_URL = f"https://api.telegram.org/bot{TOKEN}/"
+BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
+
 
 async def send_message(chat_id: int, text: str):
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(timeout=10) as client:
         await client.post(
-            BASE_URL + "sendMessage",
+            f"{BASE_URL}/sendMessage",
             json={"chat_id": chat_id, "text": text}
         )
